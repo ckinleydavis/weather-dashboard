d031c66ec6e1fdf646008d62cbfc5e2f00510fd3
Kinley Davis <d8tascientist@gmail.com>
2021-06-09 23:45:05 -0400

    update script file with api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,44 @@
-// https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
-
+// API VARIABLES
 var openWeatherAPIKey = "4eb039b0070fce508f83ad28919270e9";
-var cityName = "orlando";
-var openWeatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + openWeatherAPIKey;
+var cityName = "orlando"; // Hardcoded for testing
+var openWeatherAPIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + cityName + "&appid=" + openWeatherAPIKey;
 
-console.log(fetch(openWeatherAPIUrl));
+// Search Input & Button Variables
+var searchEl = document.querySelector('.prompt');   // document.getElementsByClassName("prompt");
+var resultsEl = document.querySelector('.results');     // document.getElementsByClassName("results");
+
+// FETCH RESULTSET
+
+    function renderMessage() {
+        var resultSet = JSON.parse( localStorage.getItem("name") );
+
+        if (resultSet !== null) {
+            // ISSUE: HOW TO DISPLAY DATA TO HTML DIV CLASS = RESULTS?
+            document.querySelector(".results").textContent = resultsEl.name;    //+ " current weather " + resultsEl.weather
+            console.log(resultSet);
+        }
+    }
+
+// var resultSet = function ( event ) {
+    // event.preventDefault();
+    fetch ( openWeatherAPIUrl ) .then ( function ( response ) {
+
+        if ( response.ok ) {
+            response.json() .then ( function ( data ) {
+                // console.log ( JSON.stringify ( data ) );
+                // ISSUE: HOW TO RETRIEVE ONE KEY/VALUE PAIR FROM DATA? (I.E. NAME: "ORLANDO")
+                // REFERENCE: https://www.sitepoint.com/loop-through-json-response-javascript/
+                // localStorage.setItem("name", JSON.stringify ( data ) ); 
+                // console.log (JSON.parse(localStorage.length));
+                renderMessage();
+            })
+        } else {
+            alert("Please enter a city name.");
+        }
+    })
+// }
+
+// searchEl.addEventListener('submit', searchCity);
+// console.log("Search Input: " + searchInput);
+// console.log("Result Set: " + resultSet);
